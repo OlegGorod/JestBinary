b2f52d26197838f27ac7da2d0a83eb8f84279081
Oleg Gorodenskyi <franky.ua@gmail.com>
2023-04-18 23:29:11 +0300

    added more tests

--- src/CartParser.test.js
+++ src/CartParser.test.js
@@ -2,6 +2,12 @@ import CartParser from './CartParser';
 import * as uuid from 'uuid';
 import fs from 'fs';
 
+const csvItem = () => {
+	const csvData = fs.readFileSync('./samples/cart.csv', 'utf8');
+	const csvArray = csvData.trim().split(/\r?\n/);
+	const csvLine = csvArray[1];
+	return csvLine
+}
 let parser;
 
 beforeEach(() => {
@@ -9,10 +15,6 @@ beforeEach(() => {
 });
 
 describe('CartParser - unit tests', () => {
-	// Add your unit tests here.
-	// test('Correctness of input data', () => {
-	// 	expect(parser.createError())
-	// })
 
 	test('Check overall sum', () => {
 		const data = [
@@ -29,6 +31,7 @@ describe('CartParser - unit tests', () => {
 			price: -50,
 			quantity: 2
 		}]
+
 		expect(parser.calcTotal(data)).toBe(500)
 		expect(parser.calcTotal([])).toBe(0)
 		expect(parser.calcTotal(dataWithOneItem)).toBe(-100)
@@ -43,29 +46,90 @@ describe('CartParser - unit tests', () => {
 				quantity: parseInt(quantity)
 			}
 		}
-		const csvData = fs.readFileSync('./samples/cart.csv', 'utf8');
-		const csvArray = csvData.trim().split(/\r?\n/);
+
+		const parsedItem = parser.parseLine(csvItem());
+		const expectedItem = createExpeсtedCardFromCSVLine(csvItem())
+
+		expect(parsedItem).toMatchObject(expectedItem);
+	})
+
+	test('verification of ID and its uniqueness', () => {
 		const allItems = []
-		const csvLine = csvArray[1];
-		const parsedItem = parser.parseLine(csvLine);
-		const expectedItem = createExpeсtedCardFromCSVLine(csvLine)
+		const parsedItem = parser.parseLine(csvItem());
 		for (let i = 0; i < 100; i++) {
-			allItems.push(parser.parseLine(csvLine))
+			allItems.push(parser.parseLine(csvItem()))
 		}
 		const uniqueId = new Set(allItems.map(item => item.id));
+		
 		expect(uniqueId.size).toBe(allItems.length)
-		expect(parsedItem).toMatchObject(expectedItem);
 		expect(parsedItem.id).toBeDefined();
 		expect(uuid.validate(parsedItem.id)).toBeTruthy()
 	})
 
-	test('function readFile reads file content correctly', () => {
+	test('function readFile() reads file content correctly', () => {
 		const testData = 'Hello world'
 		const tempFilePath = './test.txt'
+
 		fs.writeFileSync(tempFilePath, testData);
 		const result = parser.readFile(tempFilePath);
+
 		expect(result).toBe(testData)
+		fs.unlinkSync(tempFilePath);
 	});
+
+	test('function validate() returns an empty array if file is valid', () => {
+		const validFile = 'Product name,Price,Quantity\n' +
+			'Mollis consequat,9.00,2\n' +
+			'Tvoluptatem,10.32,1';
+
+		expect(parser.validate(validFile)).toEqual([])
+	})
+
+	test('function validate() returns errors if some rows have an incorrect number of cells', () => {
+		const invalidFile = 'Product name,Price,Quantity\n' +
+			'Mollis consequat,9.00\n' +
+			'Tvoluptatem,10.32,1';
+		const expectedErrors = [
+			{
+				type: parser.ErrorType.ROW,
+				rowIndex: 1,
+				columnIndex: -1,
+				message: 'Expected row to have 3 cells but received 2.',
+			},
+		];
+
+		const actualErrors = parser.validate(invalidFile)
+
+		expect(actualErrors).toHaveLength(expectedErrors.length)
+	})
+
+	test('function validate() check if some of cells have incorrect data type', () => {
+		const invalidFile = 'Product name,Price,Quantity\n' +
+			'Mollis consequat,hello world, 2\n' +
+			'Tvoluptatem,10.32,1';
+		const expectedErrors = [
+			{
+				type: parser.ErrorType.CELL,
+				rowIndex: 1,
+				columnIndex: 1,
+				message: 'Expected cell to be a positive number but received "hello world".',
+			},
+		];
+
+		const actualErrors = parser.validate(invalidFile)
+
+		expect(parser.validate(invalidFile)).toHaveLength(expectedErrors.length)
+	})
+
+	test('should throw an error if file doesn"t exist', () => {
+		const nonExistentPath = '/path/file.csv';
+		
+		expect(() => parser.parse(nonExistentPath)).toThrow()
+	})
+
+
+
+
 });
 
 describe('CartParser - integration test', () => {
